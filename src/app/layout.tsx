846c4e7cbc85f75bcf9edf5ddb8056efcc2820c4
Abdul1028 <rasoolas2003@gmail.com>
2025-06-08 01:01:48 +0530

    (feat):add analytics for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <Footer />
           <Toaster />
         </ThemeProvider>
